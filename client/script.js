c92fd07df7ac4616398669f928d49061653610db
Yomawa <bogeski.maja@gmail.com>
2016-02-25 16:08:52 -0800

    form validation

---
+++ client/script.js
@@ -0,0 +1,11 @@
+angular.module('formValidation', []).controller('mainController', function($scope){
+  $scope.submitForm = function(isValid) {
+  $scope.users = $scope.users || [];
+    event.preventDefault();
+    if (isValid) {
+      $scope.users.push($scope.user);
+      $scope.user = {};
+      $scope.userForm.$setPristine();
+    }
+  };
+});
